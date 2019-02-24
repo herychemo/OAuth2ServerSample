38a252fb97a8feaa2c01514c36272482ec0e8dd3
Heriberto Reyes Esparza <hery.chemo@gmail.com>
2019-02-23 19:57:08 -0600

    Small improvements for GetUser endpoint.

--- OAuth2ServerSample/src/main/java/com/grayraccoon/OAuth2ServerSample/data/postgres/domain/Users.java
+++ OAuth2ServerSample/src/main/java/com/grayraccoon/OAuth2ServerSample/data/postgres/domain/Users.java
@@ -1,5 +1,6 @@
 package com.grayraccoon.OAuth2ServerSample.data.postgres.domain;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
@@ -22,6 +23,7 @@ import java.util.UUID;
 })
 @NamedQueries({
         @NamedQuery(name = "Users.findAll", query = "SELECT u FROM Users u")})
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class Users implements Serializable {
 
     private static final long serialVersionUID = 1L;
